8a852d219b2c8b3b1411d9e6c7d91979ebf09d22
Mastermori <mastermoriii@gmail.com>
2023-02-25 21:09:35 +0100

    fixed project and added menus

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -3,6 +3,9 @@ extends Node3D
 
 var active := false : set = set_active
 
+func _ready() -> void:
+	$Hitbox.knockback_source_node = owner
+
 func set_active(value: bool) -> void:
 	$Hitbox.monitoring = value
 	active = value
